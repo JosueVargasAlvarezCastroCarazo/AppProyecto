d65447d7e7adb6c9084e4ba83ee2dfa83f1175fd
Josue <Josue@DESKTOP-T9P0KNS>
2023-02-17 08:45:15 -0600

    Add project files.

---
+++ AppProyecto/AppProyecto/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using AppProyecto.Services;
+using AppProyecto.Views;
+using AutoAppo_JosueVa.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppProyecto
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            //MainPage = new AppShell();
+            App.Current.UserAppTheme = OSAppTheme.Light;
+            MainPage = new NavigationPage(new AppLoginPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppProyecto/AppProyecto/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AppProyecto.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppProyecto.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
